86943a833968259828645c33b30b0e302594e2b3
joelu1234 <joelu55@yahoo.com>
2015-03-25 23:03:21 -0400

    aa

---
+++ backend/src/main/java/trading/util/CustomObjectMapper.java
@@ -0,0 +1,15 @@
+package trading.util;
+
+import java.text.SimpleDateFormat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class CustomObjectMapper extends ObjectMapper {
+
+    public CustomObjectMapper() {
+        super();
+        configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        setDateFormat(new SimpleDateFormat(Constants.DATE_FORMAT));
+    }
+}
